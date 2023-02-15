615ad4cb72fbdbc69680c3cb33c7045ef071c5d4
Adrian-Oliveira <adrianalves483@gmail.com>
2023-02-15 18:50:27 -0300

    Conponente terminado

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,24 @@ function App() {
   return (
     <div className="App">
       <Button />
-      <Button />
-      <Button variant='outline' />
-      <Button variant='outline'  />
+      <Button variant='outline' />    
       <Button variant='text'  />
-      <Button variant='text'  />
+      <Button disableShadow />
+      <Button disabled={true}/>
+      <Button variant='text' disabled={true} />
+
+      <Button startIcon='local_grocery_store' />
+      <Button endIcon='local_grocery_store' />
+
+
+      <Button size='sm'/>
+      <Button size='md'/>
+      <Button size='lg'/>
+
+      <Button color='default'/>
+      <Button color='primary'/>
+      <Button color='secondary'/>
+      <Button color='danger'/>
 
     </div>
   )
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,30 +3,52 @@ import PropTypes from 'prop-types';
 
 import './button.scss';
 
-
 interface Props {
-    label:string,
+    label?:string,
     className?:string,
-    onClick: MouseEventHandler,
-    variant: "default"|"text"|"outline"
+    onClick?: MouseEventHandler,
+    variant?: "default"|"text"|"outline",
+    disableShadow?: boolean
+    disabled?: boolean,
+    startIcon?:string,
+    endIcon?:string,
+    size?:"sm"|"md"|"lg",
+    color?: "default"|"primary"|"secondary"|"danger",
 }
 
 
-const Button = ({label, className, onClick, variant}:Props)=>{
+const Button = ({label ="Default", className="", onClick = ()=>{}, 
+                variant ="default",disableShadow = false, disabled = false,
+                startIcon= '',endIcon= '',size = "md", color="default"}:Props)=>{
     return(
         <button 
-            className={`${className} buttonComponent buttonComponent--${variant}`} 
+            disabled={disabled}
+            className={`${className} buttonComponent buttonComponent--variant-${variant}
+                        ${disableShadow?'buttonComponent--disableShadow':''}
+                        buttonComponent--size-${size}
+                        buttonComponent--color-${color}`
+                      } 
             onClick={onClick}>
-            {label}
+            
+            {startIcon?<i className="material-icons buttonComponent__startIcon">{startIcon}</i>:''}
+            <p>{label}</p>
+            {endIcon?<i className="material-icons buttonComponent__endIcon">{endIcon}</i>:''}
+
         </button>
     );
 }
 
-Button.propTypes = {
+/* Button.propTypes = {
     label: PropTypes.string.isRequired,
     className: PropTypes.string,
     onClick: PropTypes.func.isRequired,
-    variant: PropTypes.oneOf(["default","text","outline"])
+    variant: PropTypes.oneOf(["default","text","outline"]),
+    disableShadow: PropTypes.bool,
+    disabled: PropTypes.bool,
+    startIcon:PropTypes.string,
+    endIcon:PropTypes.string,
+    size: PropTypes.oneOf(["sm","md","lg"]),
+    color: PropTypes.oneOf(["default","primary","secondary","danger"])
 
 }
 
@@ -34,8 +56,14 @@ Button.defaultProps = {
     label: "Default",
     className:"",
     onClick: ()=>{},
-    variant: "default"
+    variant: "default",
+    disableShadow: false,
+    disabled: false,
+    startIcon:'',
+    endIcon:'',
+    size:"md",
+    color:"default"
 };
-
+ */
 
 export {Button};
